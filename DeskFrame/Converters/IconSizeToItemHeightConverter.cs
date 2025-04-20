7c308127ef151f06c7e190166f6e0e7c6cc944ac
btigi <btigi@users.noreply.github.com>
2025-04-20 22:04:25 +0100

    Add icon resizing

---
+++ DeskFrame/Converters/IconSizeToItemHeightConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Data;
+
+namespace DeskFrame
+{
+    public class IconSizeToItemHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is int iconSize)
+            {
+                return iconSize + 40 + iconSize / 4;
+            }
+            return 72; // 32 + 40
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DeskFrame/Converters/IconSizeToItemWidthConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Data;
+
+namespace DeskFrame
+{
+    public class IconSizeToItemWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is int iconSize)
+            {
+                return iconSize + 20;
+            }
+            return 85;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
