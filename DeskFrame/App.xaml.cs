ab1a59df96253531a003edf9316ec41672f7f2cd
Karapatakis Aggelos <aggeloskarapatakis@hotmail.com>
2025-11-09 12:06:22 +0200

    Added a check to avoid multiple instances spawning

--- DeskFrame/App.xaml.cs
+++ DeskFrame/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Toolkit.Uwp.Notifications;
+﻿using DeskFrame.Properties;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Microsoft.Win32;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Threading;
@@ -15,6 +17,22 @@ namespace DeskFrame
         public RegistryHelper reg = new RegistryHelper("DeskFrame");
         protected override void OnStartup(StartupEventArgs e)
         {
+            if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
+            {
+
+                var dialog = new Wpf.Ui.Controls.MessageBox
+                {
+                    Title = "",
+                    Content = Lang.DeskFrame_AlreadyRunning,
+                };
+
+                var result = dialog.ShowDialogAsync();
+
+                if (result.Result == Wpf.Ui.Controls.MessageBoxResult.None)
+                {
+                    Application.Current.Shutdown();
+                }
+            }
             PresentationTraceSources.DataBindingSource.Switch.Level = SourceLevels.Critical;
             base.OnStartup(e);
             ToastNotificationManagerCompat.OnActivated += ToastActivatedHandler;
