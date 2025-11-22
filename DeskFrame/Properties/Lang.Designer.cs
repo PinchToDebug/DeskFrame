8b215dd6b61bd299fadc4a431b3d0c6cabb3a2f9
PinchToDebug <52333417+PinchToDebug@users.noreply.github.com>
2025-11-22 08:00:39 +0100

    Add translation for active color label

--- DeskFrame/Properties/Lang.Designer.cs
+++ DeskFrame/Properties/Lang.Designer.cs
@@ -19,7 +19,7 @@ namespace DeskFrame.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Lang {
@@ -156,6 +156,15 @@ namespace DeskFrame.Properties {
         public static string FrameGridView_Size {
             get {
                 return ResourceManager.GetString("FrameGridView.Size", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Active color (hex).
+        /// </summary>
+        public static string FrameSettings_ActiveColor {
+            get {
+                return ResourceManager.GetString("FrameSettings.ActiveColor", resourceCulture);
             }
         }
         
