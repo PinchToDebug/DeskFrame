24d5d4e96c31808a2d5caccef07aafc847accb99
PinchToDebug <52333417+PinchToDebug@users.noreply.github.com>
2025-12-09 16:55:35 +0100

    Add grayscale effect

---
+++ DeskFrame/Shaders/GrayscaleEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Media.Effects;
+
+namespace DeskFrame.Shaders
+{
+    public class GrayscaleEffect : ShaderEffect
+    {
+        public static readonly DependencyProperty InputProperty = ShaderEffect.RegisterPixelShaderSamplerProperty("Input", typeof(GrayscaleEffect), 0);
+        public GrayscaleEffect()
+        {
+            PixelShader pixelShader = new PixelShader();
+            pixelShader.UriSource = new Uri("/DeskFrame;component/Shaders/GrayscaleEffect.ps", UriKind.Relative);
+            this.PixelShader = pixelShader;
+
+            this.UpdateShaderValue(InputProperty);
+        }
+        public Brush Input
+        {
+            get
+            {
+                return ((Brush)(this.GetValue(InputProperty)));
+            }
+            set
+            {
+                this.SetValue(InputProperty, value);
+            }
+        }
+    }
+}
